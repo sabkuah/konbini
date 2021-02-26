1aca345dc7e925f50cc01500fe07649cd2a24a1d
Sabrina Kuah <swkuah@gmail.com>
2021-02-26 14:53:24 -0800

    getAllProducts added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getProducts } from "../network";
 
 const Home = () => {
-  return <p>Hello this is home</p>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const items = await getProducts();
+      //console.log("items>>>", items);
+      await setProducts(JSON.parse(items));
+      //console.log("products>>>", products);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <p>Hello! Welcome!</p>
+
+      {products.map((p) => (
+        <p key={p.productId}>{p.productNameEn}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
--- src/network.js
+++ src/network.js
@@ -1,8 +1,15 @@
 import axios from "axios";
-
+const BASE_URL =
+  "https://bc0a6qqmm3.execute-api.ca-central-1.amazonaws.com/prod";
 //=======================
 //    Scan Products
 //=======================
+
+export async function getProducts() {
+  const response = await axios.get(`${BASE_URL}/products`);
+
+  return response.data.body;
+}
 
 //=======================
 //     Get Product
