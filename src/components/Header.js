20bd497b8fc44d3f217b361969de7bd2125d93e9
Sabrina Kuah <swkuah@gmail.com>
2021-02-27 20:44:36 -0800

    header styling, jquery/popper added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="header container ml-1 row d-flex align-items-center">
-      <h1>KONBINI</h1>
-      <p>Japanese Convenience</p>
+    <div className="header mx-1 row d-flex align-items-center justify-content-center">
+      <img className="logo" src="konbini-logo.png" alt="konbini-logo" />
+      <h1>KONBINI JAPANESE CONVENIENCE</h1>
     </div>
   );
 };
