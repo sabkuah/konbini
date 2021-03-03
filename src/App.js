3f47b849e31d63bd4cf7edb811a18c4c2957be31
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-03 01:11:06 -0800

    Add footer page and some styling

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import ProductDetail from './components/ProductDetail';
-import Navigation from './components/Navigation';
-import About from './components/About';
-import NewProduct from './components/NewProduct';
-import './App.css';
-import Header from './components/Header';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import GuardedRoute from './components/auth/GuardedRoute';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import ProductDetail from "./components/ProductDetail";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
+import NewProduct from "./components/NewProduct";
+import "./App.css";
+import Header from "./components/Header";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import GuardedRoute from "./components/auth/GuardedRoute";
+import Footer from "./components/footer";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // const authenticateUser = (authState) => {
-  //     setIsAuthenticated(authState);
-  // };
+    // const authenticateUser = (authState) => {
+    //     setIsAuthenticated(authState);
+    // };
 
-  useEffect(() => {
-    console.log(`Authenticated: ${isAuthenticated}`);
-  }, [isAuthenticated]);
+    useEffect(() => {
+        console.log(`Authenticated: ${isAuthenticated}`);
+    }, [isAuthenticated]);
 
-  return (
-    <Router>
-      <Navigation
-        isAuthenticated={isAuthenticated}
-        setIsAuthenticated={setIsAuthenticated}
-      />
-      <Header />
-      <Switch>
-        <Route path='/register'>
-          <Register setIsAuthenticated={setIsAuthenticated} />
-        </Route>
-        <Route path='/login'>
-          <Login setIsAuthenticated={setIsAuthenticated} />
-        </Route>
-        <GuardedRoute
-          path='/products/new'
-          auth={isAuthenticated}
-          component={NewProduct}
-        />
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/products/:productId'>
-          <ProductDetail isAuthenticated={isAuthenticated} />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <div className="page-container">
+            <div className="content-wrap">
+                <Router>
+                    <Navigation
+                        isAuthenticated={isAuthenticated}
+                        setIsAuthenticated={setIsAuthenticated}
+                    />
+                    <Header />
+                    <Switch>
+                        <Route path="/register">
+                            <Register setIsAuthenticated={setIsAuthenticated} />
+                        </Route>
+                        <Route path="/login">
+                            <Login setIsAuthenticated={setIsAuthenticated} />
+                        </Route>
+                        <GuardedRoute
+                            path="/products/new"
+                            auth={isAuthenticated}
+                            component={NewProduct}
+                        />
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/products/:productId">
+                            <ProductDetail isAuthenticated={isAuthenticated} />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,48 @@
+import react from "react";
+
+const Footer = () => {
+    return (
+        <div className="main-footer">
+            <div className="footer-container">
+                <div className="row">
+                    {/* Column 1 */}
+                    <div className="col">
+                        <h2 className="red-footer-title">Konbini Co.</h2>
+                        <ul className="list-style">
+                            <li>1234 Robson St </li>
+                            <li>Vanvouver BC</li>
+                            <li>Opens from 10am to 11pm everyday!</li>
+                            <li>shop@konbini.com</li>
+                            <li>604-682-3634</li>
+                        </ul>
+                    </div>
+                    {/* Column 2 */}
+                    <div className="col">
+                        <h5 className="red-footer-title">Stay in touch</h5>
+                        <ul className="list-style">
+                            <li>Instagram</li>
+                            <li>Twitter</li>
+                            <li>Facebook</li>
+                        </ul>
+                    </div>
+                    {/* Colum 3 */}
+                    <div className="col">
+                        <h5 className="red-footer-title">Find us here!</h5>
+                        {/* Geo-map?  */}
+                        <p>Map?</p>
+                    </div>
+                </div>
+                <hr />
+                {/* All right reserved / copy rights */}
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;Konbini{new Date().getFullYear()} || All Rights
+                        Reserved || Terms of service || Privacy
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
